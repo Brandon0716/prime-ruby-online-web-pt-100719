b9bfd65c038a977c720288037a6660e5beff9a88
essential-robot-4364 <brandon.james414@gmail.com>
2019-10-27 20:25:58 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,10 @@
-def prime?(n)
-  if n <= 1
+def prime?(b)
+  if b <= 1
     return false
-  elsif n <= 3
+  elsif b <= 3
     return true
   else (2..n/2).none? do |x|
-    n % x == 0
+    b % x == 0
   end
   end
 end
